4ae166cb99af55b7057c742285c11e0fe965bd38
thangpx189 <104499944+thangpx189@users.noreply.github.com>
2023-06-14 22:12:02 +0700

    Add: Accepted Sell Car, Car Detail for Booking

--- OldCarShowroomNetworkRazorPages/Pages/Index.cshtml.cs
+++ OldCarShowroomNetworkRazorPages/Pages/Index.cshtml.cs
@@ -17,70 +17,58 @@ namespace OldCarShowroomNetworkRazorPages.Pages
     public class IndexModel : PageModel
     {
         public readonly CarRepository _carRepo;
-        private readonly BOs.Models.OldCarShowroomNetworkContext _context;
+
         [BindProperty]
         public string Key { get; set; }
         public string Role { get; set; }
         public string Msg { get; set; }
-        public string Msg1 { get; set; }
+        public string Msg1 ;
 
         public IList<BOs.Models.Car> car { get; set; }
         [BindProperty]
         public string searchKey { get; set; }
 
-        public IndexModel(CarRepository carRepo, OldCarShowroomNetworkContext context)
+        public IndexModel(CarRepository carRepo)
         {
             _carRepo = carRepo;
-            _context = context;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
             if (HttpContext.Session.GetString("Key") == null || HttpContext.Session.GetString("Key") != null && HttpContext.Session.GetString("Role") != null)
-            {
-                if (_carRepo.GetAll() != null)
-                {
-                    car =  _carRepo.GetAll()
-                        .Include(c => c.CarModelYearNavigation)
-                        .Include(c => c.CarNameNavigation)
-                        .Include(c => c.ColorInsideNavigation)
-                        .Include(c => c.ColorNavigation)
-                        .Include(c => c.DriveNavigation)
-                        .Include(c => c.FuelNavigation)
-                        .Include(c => c.ManufactoryNavigation)
-                        .Include(c => c.Showroom)
-                        .Include(c => c.UsernameNavigation)
-                        .Include(c => c.VehiclesNavigation).ToList();
-                }
+            {   
+                car = await _carRepo.GetAll()
+                    .Include(c => c.CarModelYearNavigation)
+                    .Include(c => c.CarNameNavigation)
+                    .Include(c => c.ColorInsideNavigation)
+                    .Include(c => c.ColorNavigation)
+                    .Include(c => c.DriveNavigation)
+                    .Include(c => c.FuelNavigation)
+                    .Include(c => c.ManufactoryNavigation)
+                    .Include(c => c.Showroom)
+                    .Include(c => c.UsernameNavigation)
+                    .Include(c => c.Showroom.City)
+                    .Include(c => c.VehiclesNavigation)
+                    .Include(c => c.Showroom.City)
+                    .Include(c => c.Showroom.District)
+                    .Include(c => c.Showroom.WardsNavigation)
+                    .Where(c =>c.Notification == true).ToListAsync();
+                
                 return Page();
             }
             return RedirectToPage("./Login");
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
-            if (searchKey == null)
+
+
+            if (string.IsNullOrWhiteSpace(searchKey))
             {
                 Msg1 = "Vui lòng nhập tên xe để tìm kiếm";
                 return Page();
             }
             var checkCar = _carRepo.GetAll()
-                        .Include(c => c.CarModelYearNavigation)
-                        .Include(c => c.CarNameNavigation)
-                        .Include(c => c.ColorInsideNavigation)
-                        .Include(c => c.ColorNavigation)
-                        .Include(c => c.DriveNavigation)
-                        .Include(c => c.FuelNavigation)
-                        .Include(c => c.ManufactoryNavigation)
-                        .Include(c => c.Showroom)
-                        .Include(c => c.UsernameNavigation)
-                        .Include(c => c.VehiclesNavigation).Where(p => p.ManufactoryNavigation.ManufactoryName.ToLower().Contains(searchKey.ToLower()));
-            if(checkCar == null)    
-            {
-                Msg1 = "Không tìm thấy xe";
-                return Page();
-            }
-            car =  checkCar
                  .Include(c => c.CarModelYearNavigation)
                  .Include(c => c.CarNameNavigation)
                  .Include(c => c.ColorInsideNavigation)
@@ -90,7 +78,17 @@ namespace OldCarShowroomNetworkRazorPages.Pages
                  .Include(c => c.ManufactoryNavigation)
                  .Include(c => c.Showroom)
                  .Include(c => c.UsernameNavigation)
-                 .Include(c => c.VehiclesNavigation).ToList();
+                 .Include(c => c.VehiclesNavigation)
+                 .Include(c => c.Showroom.City)
+                 .Include(c => c.Showroom.District)
+                 .Include(c => c.Showroom.WardsNavigation)
+                 .Where(p => p.Notification == true && p.ManufactoryNavigation.ManufactoryName.ToLower().Contains(searchKey.ToLower().Trim()));
+            if (checkCar == null)
+            {
+                Msg1 = "Không tìm thấy xe";
+                return Page();
+            }
+            car = await checkCar.ToListAsync();
             return Page();
         }
 
