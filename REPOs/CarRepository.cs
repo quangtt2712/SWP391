70925f668950b3a5ef80b394739211eff59657e1
thangpx189 <104499944+thangpx189@users.noreply.github.com>
2023-06-15 09:57:06 +0700

    add caRepo

---
+++ REPOs/CarRepository.cs
@@ -0,0 +1,16 @@
+﻿using BOs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REPOs
+{
+    public class CarRepository : RepositoryBase<Car>
+    {
+        public CarRepository(OldCarShowroomNetworkContext context) : base(context)
+        {
+        }
+    }
+}
