216880f3dac2865d8a417e86ecd6968ab28b3ca0
taolaprovip <tranthequanganime@gmail.com>
2023-06-28 15:57:46 +0700

    update

---
+++ OldCarShowroomNetworkRazorPages/Pages/Car/Details3.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BOs.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+
+namespace OldCarShowroomNetworkRazorPages.Pages.Car
+{
+	[Authorize(Roles = "User")]
+	public class DetailsModel4 : PageModel
+	{
+		private readonly BOs.Models.OldCarShowroomNetworkContext _context;
+
+		public DetailsModel4()
+		{
+			_context = new OldCarShowroomNetworkContext();
+		}
+
+		public BOs.Models.Car car { get; set; }
+
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			car = await _context.Cars
+				.Include(c => c.CarModelYearNavigation)
+				.Include(c => c.CarNameNavigation)
+				.Include(c => c.ColorInsideNavigation)
+				.Include(c => c.ColorNavigation)
+				  .Include(c => c.ImageCars)
+				.Include(c => c.DriveNavigation)
+				.Include(c => c.FuelNavigation)
+				.Include(c => c.ManufactoryNavigation)
+				.Include(c => c.Showroom)
+				.Include(c => c.Showroom.City)
+						.Include(c => c.Showroom.District)
+						.Include(c => c.Showroom.WardsNavigation)
+				.Include(c => c.UsernameNavigation)
+				.Include(c => c.VehiclesNavigation).FirstOrDefaultAsync(m => m.CarId == id);
+
+			if (car == null)
+			{
+				return NotFound();
+			}
+			return Page();
+		}
+	}
+}
